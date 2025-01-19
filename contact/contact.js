b93761b2084b6352f014ed5305dca644c4399664
Vaibhav Babele <vaibhavbabele15@gmail.com>
2025-01-19 20:02:13 +0530

    add contact us into your website

---
+++ contact/contact.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburger').addEventListener('click', () => {
+    const navLinks = document.querySelector('.navbar-links');
+    navLinks.classList.toggle('show');
+    });
