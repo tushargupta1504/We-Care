d9492f29e5c21109af1bc9af21c3b0868bff72f3
Vaibhav Babele <vaibhavbabele15@gmail.com>
2025-02-01 20:50:21 +0530

    add dark mode in index page

---
+++ script.js
@@ -0,0 +1,9 @@
+const mode=document.getElementById("mode");
+mode.onclick=function(){
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
+        mode.src="images/sun.png";
+    }else{
+        mode.src="images/moon.png ";
+    }
+}
